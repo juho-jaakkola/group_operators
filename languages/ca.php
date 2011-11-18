6c8145fbe308f74ecc062f3b3dd12122df58b07f
Sem <sembrestels@riseup.net>
2011-11-18 17:46:56 +0100

    Normalized languages.

--- languages/ca.php
+++ languages/ca.php
@@ -1,21 +1,43 @@
 <?php
 /**
-         * Elgg Group Operators language pack
-         * 
-         * @package ElggGroupOperators
-         * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-         * @author lorea
-         * @copyright lorea
-         * @link http://lorea.cc
-         */
+ * Group Operators languages
+ *
+ * @package ElggGroupOperators
+ */
 
-$catalan = array( 
-	 'group_operators:title'  =>  "Operadors del Grup" , 
-	 'group_operators:addoperators'  =>  "Gestiona els operadors del grup" , 
-	 'group_operators:operators'  =>  "Operadors" , 
-	 'group_operators:members'  =>  "Membres" , 
-); 
+$english = array(
 
-add_translation('ca', $catalan); 
+	/**
+	 * Titles
+	 */
+	"group_operators:title" => 'Operadors de %s',
+	"group_operators:manage" => 'Maneja els operadors del grup',
+	"group_operators:operators" => 'Operadors',
+	"group_operators:members" => 'Membres',
+	
+	/**
+	 * Menus
+	 */
+	"group_operators:operators:drop" => 'Treu els privilegis',
+	"group_operators:owner" => 'Es el propietari',
+	"group_operators:owner:make" => 'Fes propietari',
+	
+	/**
+	 * Form fields
+	 */
+	"group_operators:new" => 'Afegeix un altre operador',
+	"group_operators:new:button" => 'Afegeix operador',
+	"group_operators:selectone" => 'selecciona un...',
+	
+	/**
+	 * System messages
+	 */
+	"group_operators:added" => '%s ha estat afegit satisfactòriament com operador del grup',
+	"group_operatros:add:error" => 'Ha estat impossible d\'afegir a %s com operador del grup',
+	"group_operators:owner_changed" => '%s és el nou propietari',
+	"group_operators:change_owner:error" => 'Tan sols el propietari del grup pot assignar un nou propietari',
+	"group_operators:removed" => 'L\'operador ha estat esborrat satisfactòriament',
 
-?>
+);
+
+add_translation("en", $english);
--- languages/en.php
+++ languages/en.php
@@ -1,36 +1,43 @@
 <?php
 /**
-         * Elgg Group Operators language pack
-         * 
-         * @package ElggGroupOperators
-         * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-         * @author lorea
-         * @copyright lorea
-         * @link http://lorea.cc
-         */
+ * Group Operators languages
+ *
+ * @package ElggGroupOperators
+ */
 
-	$english = array(
-		"group_operators:title" => 'Operators of %s',
-		"group_operators:manage" => 'Manage group operators',
-		"group_operators:operators" => 'Operators',
-		"group_operators:members" => 'Members',
-		"group_operators:operators:instructions" => 'click to remove operator rights',
-		"group_operators:members:instructions" => 'click to make the member group operator',
-		"group_operators:operators:drop" => 'drop privileges',
-		"group_operators:owner" => 'is the owner',
-		"group_operators:owner:make" => 'make owner',
-		"group_operators:new" => 'Add another operator',
-		"group_operators:new:instructions" => 'Write or select the name of the member of the group to make him operator.',
-		"group_operators:new:button" => 'Make operator',
-		"group_operators:selectone" => 'select one...',
-		"group_operators:added" => '%s successfully added as group operator',
-		"group_operatros:add:error" => 'It was impossible to add %s as group operator',
-		"group_operators:owner_changed" => '%s is the new owner',
-		"group_operators:change_owner:error" => 'Only the group owner can assign a new owner',
-		"group_operators:removed" => 'Operator successfully removed',
+$english = array(
 
-	);
+	/**
+	 * Titles
+	 */
+	"group_operators:title" => 'Operators of %s',
+	"group_operators:manage" => 'Manage group operators',
+	"group_operators:operators" => 'Operators',
+	"group_operators:members" => 'Members',
 	
-	add_translation("en",$english);
+	/**
+	 * Menus
+	 */
+	"group_operators:operators:drop" => 'Drop privileges',
+	"group_operators:owner" => 'Is the owner',
+	"group_operators:owner:make" => 'Make owner',
+	
+	/**
+	 * Form fields
+	 */
+	"group_operators:new" => 'Add another operator',
+	"group_operators:new:button" => 'Make operator',
+	"group_operators:selectone" => 'select one...',
+	
+	/**
+	 * System messages
+	 */
+	"group_operators:added" => '%s successfully added as group operator',
+	"group_operatros:add:error" => 'It was impossible to add %s as group operator',
+	"group_operators:owner_changed" => '%s is the new owner',
+	"group_operators:change_owner:error" => 'Only the group owner can assign a new owner',
+	"group_operators:removed" => 'Operator successfully removed',
 
-?>
+);
+
+add_translation("en", $english);
