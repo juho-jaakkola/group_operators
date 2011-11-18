af9ff5ad619fb33a5d8ab7f3b3e056408da2c960
Sem <sembrestels@riseup.net>
2011-11-18 13:34:00 +0100

    Fixed icon disapearitions when changing group owner.

--- actions/group_operators/change_owner.php
+++ actions/group_operators/change_owner.php
@@ -5,9 +5,31 @@
 	$mygroup = get_entity($mygroup_guid);
 	$who = get_entity($who_guid);
 	if ($mygroup instanceof ElggGroup && ($mygroup->owner_guid == elgg_get_logged_in_user_guid() || elgg_is_admin_logged_in())) {
+		
+		// Owner is now a simple operator
+		
 		if (!check_entity_relationship($mygroup->owner_guid, 'operator', $mygroup_guid)) {
 			add_entity_relationship($mygroup->owner_guid, 'operator', $mygroup_guid);
 		}
+		
+		// We also change icons owner
+		
+		$old_filehandler = new ElggFile();
+		$old_filehandler->owner_guid = $group->owner_guid;
+		$old_filehandler->setFilename('groups');
+		$old_path = $old_filehandler->getFilenameOnFilestore();
+		
+		$new_filehandler = new ElggFile();
+		$new_filehandler->owner_guid = $who_guid;
+		$new_filehandler->setFilename('groups');
+		$new_path = $new_filehandler->getFilenameOnFilestore();
+		
+		foreach(array('', 'tiny', 'small', 'medium', 'large') as $size) {
+			rename("$old_path/{$mygroup_guid}{$size}.jpg", "$new_path/{$mygroup_guid}{$size}.jpg");
+		}
+		
+		// Finally, we change the owner
+		
 		$mygroup->owner_guid = $who_guid;
 		$mygroup->save();
 	}
