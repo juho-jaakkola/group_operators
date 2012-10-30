4ca00b7f11e0370455554cf471748ed4503a049b
Sem <sembrestels@riseup.net>
2012-10-30 04:50:46 +0100

    Improved javascript: do not check permssions if there is no group delete button.

--- views/default/group_operators/js.php
+++ views/default/group_operators/js.php
@@ -2,7 +2,12 @@
 ?>
 
 $(function() {
-	$('.elgg-button-delete').filter('[href*="/action/groups/delete"]').toggle(
-		elgg.page_owner.owner_guid == elgg.get_logged_in_user_guid() || elgg.is_admin_logged_in()
-	);
+	var $button = $('.elgg-button-delete').filter('[href*="/action/groups/delete"]');
+	if ($button) {
+		$button.toggle(
+			elgg.page_owner &&
+			elgg.page_owner.owner_guid == elgg.get_logged_in_user_guid() ||
+			elgg.is_admin_logged_in()
+		);
+	}
 });
