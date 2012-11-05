38aa7b53c788d4049cd128651db4bf5ba7d3cb0d
Sem <sembrestels@riseup.net>
2012-11-05 06:57:50 +0100

    Refactored Groups I own page.

--- pages/group_operators/owner.php
+++ pages/group_operators/owner.php
@@ -18,19 +18,48 @@ elgg_push_breadcrumb($title);
 
 elgg_register_title_button('groups');
 
-$content = elgg_list_entities(array(
+$groups_i_own_params = array(
 	'type' => 'group',
-	'joins' => array(
-		", {$CONFIG->dbprefix}entity_relationships er",
-	),
-	'wheres' => array(
-		"((e.owner_guid = $page_owner->guid) OR (
-			er.relationship = 'operator'
-			AND er.guid_one = $page_owner->guid
-			AND er.guid_two = e.guid))",
-	),
+	'owner_guid' => $page_owner->guid,
+);
+
+$groups_i_operate_params = array(
+	'type' => 'group',
+	'relationship' => 'operator',
+	'relationship_guid' => $page_owner->guid,
 	'full_view' => false,
+);
+
+$groups_i_own_count = elgg_get_entities($groups_i_own_params + array(
+	'count' => true,
 ));
+
+$groups_i_operate_count = elgg_get_entities_from_relationship($groups_i_operate_params + array(
+	'count' => true,
+));
+
+$offset = max((int)get_input('offset'), 0);
+$entities = elgg_get_entities($groups_i_own_params + array(
+	'offset' => $offset,
+	'limit' => 10,
+));
+
+if (10 - count($entities) > 0) {
+	$entities = array_merge(
+		$entities,
+		elgg_get_entities_from_relationship($groups_i_operate_params + array(
+			'offset' => max($offset - $groups_i_own_count, 0),
+			'limit' => 10 - count($entities),
+		))
+	);
+}
+$content = elgg_view_entity_list($entities, array(
+	'full_view' => false,
+	'count' => $groups_i_own_count + $groups_i_operate_count,
+	'offset' => $offset,
+	'limit' => 10,
+));
+
 if (!$content) {
 	$content = elgg_echo('groups:none');
 }
