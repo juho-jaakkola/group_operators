10a29c182dd4499f754a41e2e526df8f65a0435d
Josh Henly <josh.henly@gmail.com>
2013-05-23 11:13:46 -0400

    Use ElggUser entity instead of guid to check group membership in add action.

--- actions/group_operators/add.php
+++ actions/group_operators/add.php
@@ -11,7 +11,7 @@ $who = get_entity(get_input('who'));
 
 $success = false;
 if ($mygroup instanceof ElggGroup && $who instanceof ElggUser && $mygroup->canEdit()) {
-	if ($mygroup->isMember($who->guid) && !check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
+	if ($mygroup->isMember($who) && !check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
 		add_entity_relationship($who->guid, 'operator', $mygroup->guid);
 		system_message(elgg_echo('group_operators:added', array($who->name, $group->name)));
 	} else {
