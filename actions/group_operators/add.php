7690e9f138c13403d5e75bd0be84096a78cffdb9
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-02-17 11:18:04 +0200

    Adds better error messages
    Tells in the error whether:
     - User is already an operator
     - User is not a member of the group

--- actions/group_operators/add.php
+++ actions/group_operators/add.php
@@ -10,11 +10,16 @@ $who = get_entity(get_input('who'));
 
 $success = false;
 if ($mygroup instanceof ElggGroup && $who instanceof ElggUser && $mygroup->canEdit()) {
-	if ($mygroup->isMember($who) && !check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
+	if (!$mygroup->isMember($who)) {
+		register_error(elgg_echo('group_operators:error:membership_required'));
+		forward(REFERER);
+	}
+
+	if (check_entity_relationship($who->guid, 'operator', $mygroup->guid)) {
+		register_error(elgg_echo('group_operators:error:already_operator', array($who->name, $group->name)));
+	} else {
 		add_entity_relationship($who->guid, 'operator', $mygroup->guid);
 		system_message(elgg_echo('group_operators:added', array($who->name, $group->name)));
-	} else {
-		register_error(elgg_echo('group_operators:add:error', array($who->name, $group->name)));
 	}
 } else {
 	register_error(elgg_echo('groups:permissions:error'));
--- languages/en.php
+++ languages/en.php
@@ -13,21 +13,21 @@ $english = array(
 	"group_operators:title" => 'Operators of %s',
 	"group_operators:manage" => 'Manage group operators',
 	"group_operators:operators" => 'Operators',
-	
+
 	/**
 	 * Menus
 	 */
 	"group_operators:operators:drop" => 'Drop privileges',
 	"group_operators:owner" => 'Is the owner',
 	"group_operators:owner:make" => 'Make owner',
-	
+
 	/**
 	 * Form fields
 	 */
 	"group_operators:new" => 'Add another operator',
 	"group_operators:new:button" => 'Make operator',
 	"group_operators:selectone" => 'select one...',
-	
+
 	/**
 	 * System messages
 	 */
@@ -36,6 +36,8 @@ $english = array(
 	"group_operators:owner_changed" => '%s is the new owner',
 	"group_operators:change_owner:error" => 'Only the group owner can assign a new owner',
 	"group_operators:removed" => 'Operator successfully removed',
+	"group_operators:error:membership_required" => 'User has to be a member of the group',
+	"group_operators:error:already_operator" => '%s is already an operator in %s',
 
 );
 
